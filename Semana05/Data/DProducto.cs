fbbfafcdd2bc19ab24c0aec4bedb77dcfef77baa
andyv <andyv@DESKTOP-B94JH0D>
2020-10-27 23:32:00 -0500

    Cambios Categorias y Productos

---
+++ Semana05/Data/DProducto.cs
@@ -0,0 +1,121 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class DProducto
+    {
+        public List<Producto> Listar(Producto producto)
+        {
+            SqlParameter[] parameters = null;
+            string comandText = string.Empty;
+            List<Producto> productos = null;
+
+            try
+            {
+                comandText = "USP_GetProducto";
+                parameters = new SqlParameter[1];
+                parameters[0] = new SqlParameter("@idproducto", System.Data.SqlDbType.Int);
+                parameters[0].Value = producto.IdProducto;
+                productos = new List<Producto>();
+
+                using (SqlDataReader reader = SQLHelper.ExecuteReader(SQLHelper.Connection,
+                    comandText, CommandType.StoredProcedure, parameters))
+                {
+                    while (reader.Read())
+                    {
+                        productos.Add(new Producto
+                        {
+                            IdProducto = reader["idproducto"] != null ? Convert.ToInt32(reader["idproducto"]) : 0,
+                            NombreProducto = reader["nombreProducto"] != null ? Convert.ToString(reader["nombreProducto"]) : string.Empty,
+                            CantidadPorUnidad = reader["cantidadPorUnidad"] != null ? Convert.ToString(reader["cantidadPorUnidad"]) : string.Empty,
+                            PrecioUnidad = reader["precioUnidad"] != null ? Convert.ToInt32(reader["precioUnidad"]) : 0
+                        });
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return productos;
+        }
+
+        public void Insertar(Producto producto)
+        {
+            SqlParameter[] parameters = null;
+            string comandText = string.Empty;
+
+            try
+            {
+                comandText = "USP_InsProducto";
+                parameters = new SqlParameter[3];
+                parameters[0] = new SqlParameter("@nombreprod", System.Data.SqlDbType.VarChar);
+                parameters[0].Value = producto.NombreProducto;
+                parameters[1] = new SqlParameter("@cantidad", System.Data.SqlDbType.VarChar);
+                parameters[1].Value = producto.CantidadPorUnidad;
+                parameters[2] = new SqlParameter("@precio", System.Data.SqlDbType.Int);
+                parameters[2].Value = producto.PrecioUnidad;
+                SQLHelper.ExecuteReader(SQLHelper.Connection, comandText, CommandType.StoredProcedure, parameters);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Actualizar(Producto producto)
+        {
+            SqlParameter[] parameters = null;
+            string comandText = string.Empty;
+
+            try
+            {
+                comandText = "USP_UdpProducto";
+                parameters = new SqlParameter[4];
+                parameters[0] = new SqlParameter("@idproducto", System.Data.SqlDbType.Int);
+                parameters[0].Value = producto.IdProducto;
+                parameters[1] = new SqlParameter("@nombreprod", System.Data.SqlDbType.VarChar);
+                parameters[1].Value = producto.NombreProducto;
+                parameters[2] = new SqlParameter("@cantidad", System.Data.SqlDbType.VarChar);
+                parameters[2].Value = producto.CantidadPorUnidad;
+                parameters[3] = new SqlParameter("@precio", System.Data.SqlDbType.Int);
+                parameters[3].Value = producto.PrecioUnidad;
+                SQLHelper.ExecuteReader(SQLHelper.Connection, comandText, CommandType.StoredProcedure, parameters);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Eliminar(int IdProducto)
+        {
+            SqlParameter[] parameters = null;
+            string comandText = string.Empty;
+
+            try
+            {
+                comandText = "USP_DelProducto";
+                parameters = new SqlParameter[1];
+                parameters[0] = new SqlParameter("@idproducto", System.Data.SqlDbType.Int);
+                parameters[0].Value = IdProducto;
+                SQLHelper.ExecuteReader(SQLHelper.Connection, comandText, CommandType.StoredProcedure, parameters);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Semana05/Semana05/ListaProducto.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Business;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Semana05
+{
+    /// <summary>
+    /// Lógica de interacción para ListaProducto.xaml
+    /// </summary>
+    public partial class ListaProducto : Window
+    {
+        public ListaProducto()
+        {
+            InitializeComponent();
+        }
+        private void BtnConsultar_Click(object sender, RoutedEventArgs e)
+        {
+            Cargar();
+        }
+
+        public void Cargar()
+        {
+            BProducto BProducto = null;
+            try
+            {
+                BProducto = new BProducto();
+                dgvProductos.ItemsSource = BProducto.Listar(0);
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error, Comunicarse con el Administrador");
+            }
+            finally
+            {
+                BProducto = null;
+            }
+        }
+
+        private void BtnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            ManProducto manProducto = new ManProducto(0);
+            manProducto.ShowDialog();
+            Cargar();
+        }
+
+        private void DgvProducto_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int idProducto;
+            var item = (Producto)dgvProductos.SelectedItem;
+            if (item == null) return;
+            idProducto = Convert.ToInt32(item.IdProducto);
+            ManProducto manProducto = new ManProducto(idProducto);
+            manProducto.ShowDialog();
+            Cargar();
+        }
+    }
+}
